6487fd8e94314c6e346e2c45fd6f69afa9ace83f
toast <29013910+ratatwiskr@users.noreply.github.com>
2025-10-20 23:06:18 +0200

    Add unit tests for getRandomColor function

---
+++ tests/utils.spec.ts
@@ -0,0 +1,13 @@
+import { getRandomColor } from "@/utils";
+
+describe("utils.ts", () => {
+  test("returns valid hex color", () => {
+    const color = getRandomColor();
+    expect(color).toMatch(/^#[0-9A-F]{6}$/i);
+  });
+
+  test("returns different colors most of the time", () => {
+    const colors = new Set(Array.from({ length: 10 }, () => getRandomColor()));
+    expect(colors.size).toBeGreaterThan(5);
+  });
+});
