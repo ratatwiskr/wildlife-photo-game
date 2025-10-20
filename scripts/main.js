603e0e616d667ca91499b5cf5caec4bbd8c2e10a
ratatwiskr <ratatwiskr@users.noreply.github.com>
2025-10-20 13:57:36 +0000

    deploy: feb0c624fa831d56bffc28b4eb16e9bec401fbc9

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { SceneRenderer } from "./sceneRenderer.js";
 import { InputHandler } from "./input.js";
+import { Config } from "./config.js";
 async function loadScene(path) {
     const res = await fetch(path);
     return await res.json();
@@ -8,7 +9,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     const viewport = document.getElementById("viewport");
     const container = document.getElementById("scene-container");
     const shutter = document.getElementById("shutter-button");
-    const scene = await loadScene("./assets/scenes/dummyScene.json");
+    const scene = await loadScene(Config.scenePath + "dummyScene.json");
     const renderer = new SceneRenderer(container, scene);
     renderer.render();
     let offset = 0;
