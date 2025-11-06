8101a6f589a6d4c8c41c147598b5a6af0dbe7d38
toast <29013910+ratatwiskr@users.noreply.github.com>
2025-11-06 22:13:39 +0100

    Add smoketests with cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:8090",
+    setupNodeEvents(on, config): void {
       // implement node event listeners here
     },
   },
---
+++ cypress/e2e/smoketests.cy.ts
@@ -0,0 +1,29 @@
+describe("Smoketests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should load the scene and display key elements", () => {
+    cy.get('[data-test-id="game-canvas"]').should("be.visible");
+    cy.get('[data-test-id="shutter-button"]').should("be.visible");
+  });
+
+  it("should switch scene", () => {
+    cy.wait(500);
+    cy.get('[data-test-id="back-to-scene-selection-button"]').click();
+
+    cy.wait(500);
+    cy.get('[data-test-id="game-canvas"]').should("not.be.visible");
+    cy.get('[data-test-id="shutter-button"]').should("not.be.visible");
+
+    cy.get(".scene-grid .scene-card").should("have.length.gte", 2);
+
+    cy.get(".scene-grid .scene-card:first").click();
+
+    cy.url().should("include", "scene=jungle_adventure_with_sun");
+
+    cy.wait(500);
+    cy.get('[data-test-id="game-canvas"]').should("be.visible");
+    cy.get('[data-test-id="shutter-button"]').should("be.visible");
+  });
+});
